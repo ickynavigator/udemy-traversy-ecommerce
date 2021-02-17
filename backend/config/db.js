6a1f22ec7ce1db60e56836a53fac50e10faebf43
ickynavigator <gabrielobi.of@gmail.com>
2021-02-17 20:00:17 +0200

    Episode 21

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,9 +8,9 @@ const connectDB = async () => {
       useCreateIndex: true,
     });
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+    console.log(`\nMongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
-    console.error(`Error: ${error.message}`.red.underline.bold);
+    console.error(`\nError: ${error.message}`.red.underline.bold);
     process.exit(1);
   }
 };
