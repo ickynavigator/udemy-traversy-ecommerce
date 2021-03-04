2cd0622b0fff76c31e52b2033a061942e0956367
ickynavigator <gabrielobi.of@gmail.com>
2021-03-04 21:57:47 +0200

    Add frontend folder back to git repo

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(folder, "/frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(folder, "..", "frontend", "build", "index.html"))
   );
 }
 
