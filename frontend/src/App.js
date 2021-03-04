c6237c556fcc15e3c9fcd9070966ae2fc36c6de7
ickynavigator <gabrielobi.of@gmail.com>
2021-03-04 15:21:24 +0200

    Episode 85

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -40,6 +40,7 @@ const App = () => {
           <Route path="/admin/product/:id/edit" component={ProductEditScreen} />
           <Route path="/admin/userlist" component={UserListScreen} />
           <Route path="/admin/user/:id/edit" component={UserEditScreen} />
+          <Route path="/search/:keyword" component={HomeScreen} />
           <Route path="/" exact component={HomeScreen} />
         </Container>
       </main>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,9 @@
+import { Route } from "react-router-dom";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { LinkContainer } from "react-router-bootstrap";
 import { logout } from "../actions/userActions";
+import SearchBox from "./SearchBox";
 
 const Header = ({ location }) => {
   const userLogin = useSelector((state) => state.userLogin);
@@ -25,6 +27,7 @@ const Header = ({ location }) => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
           <Navbar.Collapse id="basic-navbar-nav">
+            <Route render={({ history }) => <SearchBox history={history} />} />
             <Nav className="ml-auto">
               <LinkContainer to="/cart">
                 <Nav.Link>
